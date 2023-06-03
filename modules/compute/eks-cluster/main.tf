f20d03ac882e35b55d047b5e2f1a46dad001e018
Moses Lusengeri Anzagi <lusengeri@gmail.com>
2023-06-03 17:19:34 +0300

    add eks-cluster module

---
+++ modules/compute/eks-cluster/main.tf
@@ -0,0 +1,22 @@
+module "control_plane" {
+  source = "../eks-control-plane"
+
+  namespace      = var.namespace
+  subnet_ids     = var.subnet_ids
+  vpc_id         = var.vpc_id
+  workers_sg_ids = [module.workers.workers_sg_id]
+}
+
+module "workers" {
+  source = "../eks-workers"
+
+  control_plane_security_group_id = module.control_plane.control_plane_sg_id
+  desired_worker_node_no          = var.desired_worker_node_no
+  max_worker_node_no              = var.max_worker_node_no
+  min_worker_node_no              = var.min_worker_node_no
+  namespace                       = var.namespace
+  subnet_ids                      = var.subnet_ids
+  vpc_id                          = var.vpc_id
+  worker_instance_type            = var.worker_instance_type
+  worker_key_name                 = var.worker_key_name
+}
---
+++ modules/compute/eks-cluster/variables.tf
@@ -0,0 +1,31 @@
+variable "desired_worker_node_no" {
+  type = number
+}
+
+variable "max_worker_node_no" {
+  type = number
+}
+
+variable "min_worker_node_no" {
+  type = number
+}
+
+variable "namespace" {
+  type = string 
+}
+
+variable "subnet_ids" {
+  type = list(string) 
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "worker_instance_type" {
+  type = string 
+}
+
+variable "worker_key_name" {
+  type = string
+}
