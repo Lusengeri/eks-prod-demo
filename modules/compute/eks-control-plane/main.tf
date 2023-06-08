378e2b96b2b1fa96018d491e8bc0ad60f231fd47
Moses Lusengeri Anzagi <lusengeri@gmail.com>
2023-06-08 19:42:17 +0300

    enable endpoint private access for eks cluster

--- modules/compute/eks-control-plane/main.tf
+++ modules/compute/eks-control-plane/main.tf
@@ -1,11 +1,13 @@
 resource "aws_eks_cluster" "app_cluster" {
   name     = "${var.namespace}-eks-cluster"
   role_arn = aws_iam_role.eks_cluster_iam_role.arn
-  version  = "${var.kubernetes_version}"
+  version  = var.kubernetes_version
 
   vpc_config {
-    subnet_ids         = var.subnet_ids
-    security_group_ids = [aws_security_group.control_plane_sg.id]
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    subnet_ids              = var.subnet_ids
+    security_group_ids      = [aws_security_group.control_plane_sg.id]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
