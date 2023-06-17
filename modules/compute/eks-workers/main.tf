26f4c1fce29a6042e0ce546028ff87bfae8f4168
Moses Lusengeri Anzagi <lusengeri@gmail.com>
2023-06-17 09:07:13 +0300

    supply worker_node_no parameters to node group resource

--- modules/compute/eks-workers/main.tf
+++ modules/compute/eks-workers/main.tf
@@ -4,16 +4,17 @@ data "aws_ssm_parameter" "eks_ami_release_version" {
 
 resource "aws_eks_node_group" "worker_node_group" {
   cluster_name    = var.cluster_full_name
-  #version         = aws_eks_cluster.example.version
+  version         = var.cluster_version
+  instance_types  = [var.worker_instance_type]
   node_group_name = "${var.namespace}-worker-node-group"
   node_role_arn   = aws_iam_role.eks_worker_role.arn
   release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_release_version.value)
   subnet_ids      = var.subnet_ids
 
   scaling_config {
-    desired_size = 3
-    max_size     = 4
-    min_size     = 2
+    desired_size = var.desired_worker_node_no
+    max_size     = var.max_worker_node_no 
+    min_size     = var.min_worker_node_no 
   }
 
   update_config {
@@ -28,83 +29,3 @@ resource "aws_eks_node_group" "worker_node_group" {
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
 }
-#resource "aws_autoscaling_group" "eks_node_asg" {
-#  name                      = "${var.cluster_full_name}-workers-asg-${var.worker_instance_type}"
-#  desired_capacity          = var.desired_worker_node_no
-#
-#  launch_template {
-#    id = aws_launch_template.eks_node_launch_template.id
-#    version = "$Latest"
-#  }
-#
-#  lifecycle {
-#    create_before_destroy = true
-#  }
-#
-#  max_size            = var.max_worker_node_no
-#  min_size            = var.min_worker_node_no
-#  vpc_zone_identifier = var.subnet_ids
-#
-#  tag {
-#    key                 = "Name"
-#    value               = "${var.namespace}-workers-${var.worker_instance_type}"
-#    propagate_at_launch = true
-#  }
-#  
-#  tag {
-#    key                 = "kubernetes.io/cluster/${var.cluster_full_name}"
-#    value               = "owned"
-#    propagate_at_launch = true
-#  }
-#
-#  #timeouts {
-#  #  delete = "25m"
-#  #}
-#}
-#
-#resource "aws_launch_template" "eks_node_launch_template" {
-#  name = "eks-node-launch-template"
-#
-#  block_device_mappings {
-#    device_name = "/dev/sdf"
-#
-#    ebs {
-#      volume_type = "gp2"
-#      volume_size = 100 
-#      delete_on_termination = true
-#    }
-#  }
-#
-#  iam_instance_profile {
-#    arn = aws_iam_instance_profile.eks_worker_instance_profile.arn
-#  }
-#
-#  image_id      = var.worker_ami_id
-#  instance_type = var.worker_instance_type
-#  key_name      = var.worker_key_name
-#
-#  lifecycle {
-#    create_before_destroy = true 
-#  }
-#
-#  monitoring {
-#    enabled = true
-#  }
-#
-#  #network_interfaces {
-#  #  associate_public_ip_address = false
-#  #  security_groups = ["${aws_security_group.workers_sg.id}"]
-#  #}
-#
-#  vpc_security_group_ids = [aws_security_group.workers_sg.id]
-#
-#  tag_specifications {
-#    resource_type = "instance"
-#
-#    tags = {
-#      Name = "${var.namespace}-worker-node"
-#    }
-#  }
-#
-#  user_data = base64encode(data.template_file.worker_node_user_data_template.rendered)
-#}
