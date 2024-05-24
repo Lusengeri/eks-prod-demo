7f0cc0c0b35485af124582b73e461ddf02e7dbb2
Lusengeri <lusengeri@gmail.com>
2024-05-24 19:40:32 +0300

    add deployment stage parameter in NAT module

--- terraform_iac/modules/networking/nat/variables.tf
+++ terraform_iac/modules/networking/nat/variables.tf
@@ -14,6 +14,15 @@ variable "private_subnet_id" {
   type = any
 }
 
+variable "stage" {
+  type = string 
+    
+  validation {
+    condition     = contains(["tst", "dev", "stg", "prd"], var.stage)
+    error_message = "The specified deployment stage must be one of 'tst', 'dev', 'stg', or 'prd'."
+  }
+}
+
 variable "vpc_id" {
   type = string 
 }
